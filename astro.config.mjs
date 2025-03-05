4c2464b3a9d195189e71a4ba422a46119a4f6d44
Ramalingasamy M K <ramalinga-16192@ramalinga-16192.csez.zohocorpin.com>
2025-03-05 17:46:21 +0530

    new

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://0xraminfosec.github.io/",
+  site: "https://0xram.com/",
   base: "/",
   trailingSlash: "always",
   integrations: [
